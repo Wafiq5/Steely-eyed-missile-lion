ee6299af6bd6a7aba0f38ccd96d199f426667003
wafiq <aswafiq8100@gmail.com>
2025-09-20 21:19:43 -0500

    Added SEMLTeleop_NormalDrive (implements bearing alignment)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SEMLTeleop_NormalDrive.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+@TeleOp(name="SELMTeleop_NormalDrive", group="SEML")
+public class SEMLTeleop_NormalDrive extends OpMode{
+    public double drive, turn, strafe;
+
+    public DcMotor backLeftDrive, backRightDrive, frontLeftDrive, frontRightDrive;
+    public Limelight3A limelight;
+
+    Double lastTx = null;
+    boolean bearingAlignMode = false;
+
+    double speed = 0.5;
+
+    @Override
+    public void init() {
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.start();
+
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+    /**
+     * Code to run ONCE when the driver hits START
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /**
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        //* Fine tuning bearing
+        double bearingThreshold = 1;
+        double Kp = 0.03;
+
+        //* Toggle bearing align mode
+        boolean gamepadAState = gamepad1.a;
+        if (gamepad1.a && !gamepadAState) {
+            bearingAlignMode = !bearingAlignMode;
+        }
+
+        //* Normal Drive (Turn w/ left joystick)
+        drive = gamepad1.right_stick_y;
+        strafe = gamepad1.right_stick_x;
+        turn = gamepad1.left_stick_x;
+
+        LLResult result = limelight.getLatestResult();
+
+        if (result != null && result.isValid()) {
+            bearingAlign(result, Kp, bearingThreshold);
+        } else{
+            // Look for target
+            if (bearingAlignMode) {
+                if(lastTx >= 0){
+                    strafe = speed;
+                }else if(lastTx < 0){
+                    strafe = - speed;
+                }
+            }
+        }
+
+        move(speed, drive, strafe, turn);
+
+        telemetry.addData("Align mode active", bearingAlignMode);
+        telemetry.addData("LastTX", lastTx);
+        telemetry.addData("Result", result != null && result.isValid());
+        telemetry.update();
+    }
+
+    /*! Methods*/
+    /**
+     * Movement function for mecanum drive
+     * @param speed overall speed multiplier
+     * @param drive forward/backward motion
+     * @param strafe left/right strafe
+     * @param turn rotation motion
+     * <ul>
+     *     <li><b>+ values go forward , turn right, and strafe right</b></li>
+     *     <li><b>- values go backward, turn left , and strafe left<b></li>
+     * </ul>
+     */
+    public void move(double speed, double drive, double strafe, double turn){
+        double frontLeftPower = drive - strafe - turn;
+        double frontRightPower = drive + strafe + turn;
+        double backLeftPower = - drive + strafe - turn;
+        double backRightPower = - drive - strafe + turn;
+
+        double max = Math.max(1.0,
+                Math.max(Math.abs(frontLeftPower),
+                        Math.max(Math.abs(frontRightPower),
+                                Math.max(Math.abs(backLeftPower),
+                                        Math.abs(backRightPower)))));
+
+        frontLeftPower /= max;
+        frontRightPower /= max;
+        backLeftPower /= max;
+        backRightPower /= max;
+
+        frontLeftDrive.setPower(speed * (frontLeftPower));
+        frontRightDrive.setPower(speed * (frontRightPower));
+        backLeftDrive.setPower(speed * (backLeftPower));
+        backRightDrive.setPower(speed * (backRightPower));
+    }
+
+    /**
+     * Bearing alignment function
+     * @param result Latest Limelight result
+     * @param Kp Proportional constant for steering
+     * @param bearingThreshold Acceptable error range in degrees
+     */
+    public void bearingAlign(LLResult result, double Kp, double bearingThreshold){
+        double tx = result.getTx();
+
+        if (bearingAlignMode) {
+            strafe = 0;
+            telemetry.addData("tx", tx);
+            telemetry.update();
+
+            if (Math.abs(tx) > bearingThreshold) {
+                strafe = Kp * tx;
+                strafe = Math.max(- speed, Math.min(speed, strafe)); // clamp
+            }
+        }
+
+        //* Telemetry
+        lastTx = tx;
+        telemetry.addData("Target X", tx);
+    }
+
+    /**
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+
+    }
+}
